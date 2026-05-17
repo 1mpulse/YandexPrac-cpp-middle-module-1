45c454cd9f4a9097faa43f82210669e730571f55
1mpulse <potapov.dima-2@yandex.ru>
2026-05-17 14:13:08 +0300

    add PImpl idiom

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,40 @@
+#include "crypto_guard_ctx.h"
+
+#include <iostream>
 
 namespace CryptoGuard {
+
+class CryptoGuardCtx::Impl
+{
+public:
+    Impl() = default;
+    ~Impl() = default;
+};
+
+CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<Impl>())
+{
+
+}
+
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+
+CryptoGuardCtx::CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
+
+CryptoGuardCtx &CryptoGuardCtx::operator=(CryptoGuardCtx &&) noexcept = default;
+
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
+{
+
+}
+
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
+{
+
+}
+
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream)
+{
+    return "NOT_IMPLEMENTED";
+}
 
 }  // namespace CryptoGuard
