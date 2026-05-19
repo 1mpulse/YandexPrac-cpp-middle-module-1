ca5d15247ff7b897e6a96c6fa746c04240208f37
1mpulse <potapov.dima-2@yandex.ru>
2026-05-19 17:44:12 +0300

    logic CryptoGuardCtx::PImpl::EncryptFile

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <algorithm>
 #include <array>
+#include <iostream>
+#include <iterator>
+#include <memory>
 #include <stdexcept>
+#include <vector>
 
 #include <openssl/evp.h>
 
@@ -62,15 +66,82 @@ CryptoGuardCtx &CryptoGuardCtx::operator=(CryptoGuardCtx &&) noexcept = default;
 
 void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
 {
-
+    pImpl_->EncryptFile(inStream, outStream, password);
 }
 
 void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
 {
-
+    pImpl_->DecryptFile(inStream, outStream, password);
 }
 
 std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream)
+{
+    return pImpl_->CalculateChecksum(inStream);
+}
+
+void CryptoGuardCtx::PImpl::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
+{
+    if (!inStream.good()) {
+        throw std::runtime_error{"Bad input"};
+    }
+
+    if (!outStream.good()) {
+        throw std::runtime_error{"Bad output"};
+    }
+
+    auto params = CreateChiperParamsFromPassword(password);
+    std::string input((std::istreambuf_iterator<char>(inStream)), std::istreambuf_iterator<char>());
+
+    if (!inStream.good())
+    {
+        throw std::runtime_error{"Bad input #2"};
+    }
+
+    int outLen = 0;
+    int tmpLen = 0;
+    std::vector<unsigned char> outBuf(input.size() + EVP_MAX_BLOCK_LENGTH);
+
+    auto *ctx = EVP_CIPHER_CTX_new();
+    if(!ctx)
+    {
+        throw std::runtime_error{"Failed to create EVP_CIPHER_CTX"};
+    }
+
+    if (EVP_EncryptInit_ex(ctx, params.cipher, nullptr, params.key.data(), params.iv.data()) != 1)
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        throw std::runtime_error{"Failed EVP_EncryptInit_ex"};
+    }
+
+    if (EVP_EncryptUpdate(ctx, outBuf.data(), &outLen, reinterpret_cast<const unsigned char *>(input.data()), static_cast<int>(input.size())) != 1)
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        throw std::runtime_error{"Failed EVP_EncryptUpdate"};
+    }
+
+    if (EVP_EncryptFinal_ex(ctx, outBuf.data() + outLen, &tmpLen) != 1)
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        throw std::runtime_error{"Failed EVP_EncryptFinal_ex"};
+    }
+
+    outLen += tmpLen;
+    EVP_CIPHER_CTX_free(ctx);
+
+    outStream.write(reinterpret_cast<const char *>(outBuf.data()), outLen);
+
+    if (!outStream.good())
+    {
+        throw std::runtime_error{"Bad output #3"};
+    }
+}
+
+void CryptoGuardCtx::PImpl::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
+{
+
+}
+
+std::string CryptoGuardCtx::PImpl::CalculateChecksum(std::iostream &inStream)
 {
     return "NOT_IMPLEMENTED";
 }
--- tests/crypo_guard_ctx_test.cpp
+++ tests/crypo_guard_ctx_test.cpp
@@ -1,3 +1,42 @@
 #include <gtest/gtest.h>
 
-TEST(TestComponentName, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+#include <sstream>
+#include <string>
+
+#include "crypto_guard_ctx.h"
+
+TEST(CryptoGuardEncrypt, Test1)
+{
+    CryptoGuard::CryptoGuardCtx ctx;
+
+    std::stringstream input("hello world!");
+    std::stringstream output;
+
+    ctx.EncryptFile(input, output, "password");
+
+    EXPECT_FALSE(output.str().empty());
+}
+
+TEST(CryptoGuardEncrypt, Test2)
+{
+    CryptoGuard::CryptoGuardCtx ctx;
+
+    std::stringstream input("hello world!");
+    input.setstate(std::ios::badbit);
+
+    std::stringstream output;
+
+    ASSERT_THROW(ctx.EncryptFile(input, output, "password"), std::runtime_error);
+}
+
+TEST(CryptoGuardEncrypt, Test3)
+{
+    CryptoGuard::CryptoGuardCtx ctx;
+
+    std::stringstream input("hello world!");
+
+    std::stringstream output;
+    output.setstate(std::ios::badbit);
+
+    EXPECT_THROW(ctx.EncryptFile(input, output, "password"), std::runtime_error);
+}
